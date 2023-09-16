59beddf24cff909b08c714512b87ed906e099338
Menyus11 <senkialfonz11@gmail.com>
2023-09-16 21:10:35 +0200

    todo 1.2

--- frontend/src/components/UserNav.jsx
+++ frontend/src/components/UserNav.jsx
@@ -32,7 +32,7 @@ const UserNav = () => {
 
     return (
         <div className='courgette'>
-            <nav className="navbar navbar-expand-lg bg-green ">
+            <nav className="navbar navbar-expand-lg bg-lightGreen ">
                 <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
